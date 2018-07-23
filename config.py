7e9a6d26e89ca356f99b8a94acdcdc747d838b5f
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-07-23 02:14:51 -0400

    Initial commit:
    - Menu:
      - Show menu prompt if now command line arguments supplied
      - TODO: detect command line arguments and run corresponding commands
    - Config file
      - Add MEMBERS var to hold name and GH username
      - Add ROOT_DIR var to hold root local directory to clone to
      - Add BASE_URL var to hold github url
      - TODO: Allow modifying config from menu prompt
    - Clone repos functionality
      - Show prompt to input repo name
      - Clone repo for each member into corresponding folder inside ROOT_DIR
    - Pull repos functionality
      - Show prompt to input repo name
      - CD into repo and run 'git pull' for each member in MEMBERS var

---
+++ config.py
@@ -0,0 +1,13 @@
+MEMBERS = {
+    'danny': 'd-vail',
+    'il-ho': 'ilhoKim',
+    'ja': 'JSNavey',
+    'maribel': 'maribelcuales',
+    'erinc': 'erinc35',
+    'andrew': 'AKolumbic'
+}
+
+ROOT_DIR = '~/programming/sandbox/test'
+
+
+BASE_URL = 'https://github.com'
---
+++ git-herd.py
@@ -0,0 +1,70 @@
+import subprocess
+import sys
+
+from config import *
+
+arguments = sys.argv[1:]
+running = True
+
+
+def add_member(name, username):
+    members[name] = username
+
+
+def clone_repos(repo_name):
+    commands = []
+    for member in MEMBERS:
+        repo_url = f'{BASE_URL}/{MEMBERS[member]}/{repo_name}.git'
+        repo_dest = f'{ROOT_DIR}/{member}/{repo_name}'
+        commands.append(f'git clone {repo_url} {repo_dest}')
+    command =';'.join(commands)
+    subprocess.call(command, shell=True)
+
+
+def pull_repos(repo_name):
+    commands = []
+    for member in MEMBERS:
+        sub_dir = f'{ROOT_DIR}/{member}/{repo_name}'
+        commands.append(f'cd {sub_dir}')
+    command = ';git pull;'.join(commands)
+    subprocess.call(command, shell=True)
+
+
+if len(arguments) is 0:
+    while running:
+        print('')
+        # print('--------------------------')
+        print('  c: clone a projects repos')
+        print('  p: pull all repos')
+        print('')
+        # print('  s: set target directory')
+        # print('  a: add member')
+        print('  m: view members')
+        print('  r: view root directory')
+        print('')
+        print('  q: quit')
+        print('')
+        choice = input('>>> ')
+
+        if choice == 'c':
+            repo_name = input('enter repo name: ')
+            clone_repos(repo_name)
+        if choice == 'p':
+            repo_name = input('enter repo name: ')
+            pull_repos(repo_name)
+        # if choice == 'a':
+        #     name = input('member name: ')
+        #     username = input('github username: ')
+        #     add_member(name, username)
+        if choice == 'm':
+            # print('--------------------------')
+            print('')
+            print('Members: ', MEMBERS)
+            print('')
+        if choice == 'r':
+            print('')
+            print('Root directory: ', ROOT_DIR)
+            print('')
+        if choice == 'q':
+            running = False
+
