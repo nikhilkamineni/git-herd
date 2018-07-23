3933c5f8e6610f32ab2ad830c6ef1067cd996c3d
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-07-23 03:00:57 -0400

    Add example-config.py; remove/ignore config.py

---
+++ example-config.py
@@ -0,0 +1,14 @@
+MEMBERS = {
+    # Add your group members here. 
+    # The value of 'name' will be the name of the subdirectory created
+    # For example:
+    # 'name': 'github-handle'
+}
+
+# Location that your members sub-directories will be created
+# Note: don't add a trailing '/'
+ROOT_DIR = '~/herd'
+
+# URL of the hosting service you're using
+# Note: don't add a trailing '/'
+BASE_URL = 'https://github.com'
